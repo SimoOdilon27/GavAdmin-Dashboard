eb9e4dad9d26bb95f76640dd9e0f97d5e07c6905
Odilon <simoodilon60@gmail.com>
2024-08-17 16:03:39 +0100

    teller

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -168,13 +168,13 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-            <Item
+            {/* <Item
               title="Teller"
               to="/teller"
               icon={<GroupIcon />}
               selected={selected}
               setSelected={setSelected}
-            />
+            /> */}
 
             <Typography
               variant="h6"
